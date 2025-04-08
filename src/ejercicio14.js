b007ef4d80c78148aba2765d1c18d3852bb0635b
ferna <fernandodl15@outlook.com>
2025-04-07 23:49:01 -0600

    ejercicio14

---
+++ src/ejercicio14.js
@@ -0,0 +1,17 @@
+/*
+14. Descuento por edad
+
+Tema: condición múltiple
+
+let edad = 68
+Muestra "Descuento especial" si tiene menos de 12 o más de 65 años. De lo contrario, muestra "Sin descuento".
+*/
+
+let edad = 68
+
+if (edad < 12 || edad > 65) {
+    console.log("Tienes un descuento especial")
+}
+else {
+    console.log("No tienes ningun descuento")
+}
