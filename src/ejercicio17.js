354b618d2f559c8b9f58ea391468a266fcebffc8
ferna <fernandodl15@outlook.com>
2025-04-07 23:57:04 -0600

    ejercicio16

---
+++ src/ejercicio17.js
@@ -0,0 +1,24 @@
+/*
+17. Mayor de tres números
+
+Tema: if-else anidado
+
+let a = 10,
+    b = 5,
+    c = 7
+Muestra cuál es el número mayor entre los tres.
+*/
+
+let a = 10
+let b = 5
+let c = 7
+
+if (a > b && a > c) {
+    console.log(`El número mayor es ${a}`)
+}
+else if (b > a && b > c) {
+    console.log(`El número mayor es ${b}`)
+}
+else if (c > a && c > b) {
+    console.log(`El número mayor es ${c}`)
+}
