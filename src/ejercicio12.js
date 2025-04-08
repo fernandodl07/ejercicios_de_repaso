5383c4a47c7407ccdf1d71322de0c089381fbedd
ferna <fernandodl15@outlook.com>
2025-04-07 23:45:27 -0600

    ejercicio12

---
+++ src/ejercicio12.js
@@ -0,0 +1,17 @@
+/*
+Básicos
+12. Mayoría de edad
+
+Tema: if-else
+
+let edad = 17
+Si la edad es mayor o igual a 18, muestra "Eres mayor de edad". Si no, muestra "Eres menor de edad".
+*/
+
+let edad = 17
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad")
+} else {
+    console.log("Eres menor de edad")
+}
