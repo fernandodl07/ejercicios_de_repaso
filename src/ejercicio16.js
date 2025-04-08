c7c54e9d39af880a40b8c4c3bf6fff9f765ab796
ferna <fernandodl15@outlook.com>
2025-04-07 23:54:44 -0600

    ejercicio16

---
+++ src/ejercicio16.js
@@ -0,0 +1,18 @@
+/*
+16. Ingreso a evento
+
+Tema: operadores lógicos
+
+let tieneEntrada = true
+let estaEnLista = false
+Muestra "Puede ingresar" solo si tiene entrada y está en lista. Si no, muestra "Acceso denegado".
+ */
+
+let tieneEntrada = true
+let estaEnLista = false
+
+if (tieneEntrada && estaEnLista) {
+    console.log("Puede ingresar")
+} else {
+    console.log("El aacceso fue denegado")
+}
