833850bd6cb30870b508b185601d8facea040455
ferna <fernandodl15@outlook.com>
2025-04-07 23:26:14 -0600

    ejercicio4

---
+++ src/ejercicio4.js
@@ -0,0 +1,18 @@
+/*
+Intermedios
+4. Sumar texto numérico convertido
+
+Tema: conversión de string a number
+
+let edadTexto = '25'
+Convierte la variable a número y suma 5.
+
+Imprime el resultado final:
+
+"Edad más 5: 30"
+*/
+
+let edadTexto = '25'
+let edad = Number(edadTexto) + 5    
+
+console.log(`Edad ${edadTexto} mas 5: ${edad}`)  
