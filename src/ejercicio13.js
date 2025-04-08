f40caf9536a2d25674aa80d77f510eb58951995b
ferna <fernandodl15@outlook.com>
2025-04-07 23:46:45 -0600

    ejercicio13

---
+++ src/ejercicio13.js
@@ -0,0 +1,17 @@
+/*
+Calificación mínima
+
+Tema: if-else
+
+let nota = 55
+Si la nota es mayor o igual a 60, imprime "Aprobado". Si no, imprime "Reprobado"
+*/
+
+let nota = 55
+
+if (nota >= 60) {
+    console.log("Aprobado")
+}
+else {
+    console.log("Reprobado")
+}
