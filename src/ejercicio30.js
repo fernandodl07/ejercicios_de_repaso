1cb8648e03d2054bd55bf12c26fce30b4239c7c2
ferna <fernandodl15@outlook.com>
2025-04-08 14:04:04 -0600

    ejercicio30

--- src/ejercicio30.js
+++ src/ejercicio30.js
@@ -8,7 +8,7 @@ Suma todos los elementos del arreglo usando for.
 */
 
 const numeros = [5, 10, 15, 20]
-let suma 
+let suma = 0
 for (let i = 0; i < numeros.length; i++) {
     suma += numeros[i]
 }
