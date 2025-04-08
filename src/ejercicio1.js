c80a2185ae7c86504daf4a60749d180f064706c1
ferna <fernandodl15@outlook.com>
2025-04-07 23:19:49 -0600

    ejercicio1

---
+++ src/ejercicio1.js
@@ -0,0 +1,16 @@
+/*
+Básicos
+1. Edad del usuario
+
+Tema: declaración de variable
+
+Declara una variable llamada edad y asígnale el valor 21.
+
+Muestra un mensaje en consola que diga:
+
+
+
+*/
+
+let edad = 21 
+console.log(`Tu edad es: ${edad} años`)
