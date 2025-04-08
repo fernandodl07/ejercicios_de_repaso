f8baf4cd88773951af9f34b342d205ae397df3e5
ferna <fernandodl15@outlook.com>
2025-04-07 23:33:17 -0600

    ejercicio8

---
+++ src/ejercicio8.js
@@ -0,0 +1,13 @@
+/*
+Conversión de temperatura
+
+Tema: expresiones matemáticas
+
+let celsius = 30
+Convierte a Fahrenheit usando la fórmula: (celsius × 9/5) + 32
+*/
+
+let celsius = 30
+let fahrenheit = (celsius * 9/5) + 32
+
+console.log(`La temperatura en celcius es ${celsius} y en fahrenheit es ${fahrenheit}`)
