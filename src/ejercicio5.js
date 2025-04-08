3b07bdcf9891a12d8d4c8ce1c409bb6c2800844b
ferna <fernandodl15@outlook.com>
2025-04-07 23:27:44 -0600

    ejercicio5

---
+++ src/ejercicio5.js
@@ -0,0 +1,14 @@
+/*
+Tipos especiales: null y undefined
+
+Tema: valores especiales
+
+let valorNulo = null
+let valorIndefinido
+Muestra el tipo de cada uno usando typeof
+*/
+
+let valorNulo = null
+let valorIndefinido
+console.log(`El tipo de dato de valorNulo es ${typeof valorNulo}`)
+console.log(`El tipo de dato de valorIndefinido es ${typeof valorIndefinido}`)
