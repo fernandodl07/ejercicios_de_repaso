c8766aa2b7e887eadc1f2933ba3bffa546a8e9f0
ferna <fernandodl15@outlook.com>
2025-04-08 18:02:28 -0600

    ejercicio49

---
+++ src/ejercicio49.js
@@ -0,0 +1,14 @@
+/*
+49. Combinar listas de invitados
+
+Tema: concat()
+
+const invitadosA = ['Carlos', 'Lucía']
+const invitadosB = ['Pedro', 'Sofía']
+Combina ambos arreglos en uno nuevo y muéstralo.
+*/
+
+const invitadosA = ['Carlos', 'Lucía']
+const invitadosB = ['Pedro', 'Sofía']   
+const invitadosCombinados = invitadosA.concat(invitadosB)
+console.log(invitadosCombinados)
