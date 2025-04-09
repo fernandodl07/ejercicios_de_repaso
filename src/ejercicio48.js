056ac9933efdebde9928001d2f2a86e62c25192e
ferna <fernandodl15@outlook.com>
2025-04-08 18:01:26 -0600

    ejercicio48

---
+++ src/ejercicio48.js
@@ -0,0 +1,13 @@
+/*
+48. Verificar si un producto está en la lista
+
+Tema: includes()
+
+const productos = ['Mouse', 'Teclado', 'Monitor']
+Verifica si "Monitor" está en el arreglo.
+
+Muestra true o false.
+*/
+const productos = ['Mouse', 'Teclado', 'Monitor']
+const existeMonitor = productos.includes('Monitor')
+console.log(existeMonitor)
